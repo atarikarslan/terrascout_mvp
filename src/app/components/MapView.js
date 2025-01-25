c2c487f9019f556489e48c5152d02172b8dbeb1d
Tarik Arslan <103arslan@gmail.com>
2025-01-25 03:25:59 +0100

    map func 3

--- src/app/components/MapView.js
+++ src/app/components/MapView.js
@@ -1,42 +1,37 @@
 // src/components/MapView.js
+import * as L from 'leaflet';
 import { useEffect, useRef } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const MapView = ({ selectedLocation, onLocationSelect, sampleLocations }) => {
   const mapRef = useRef(null);
 
   useEffect(() => {
-    // This code will only run in the browser, not during server-side rendering
     if (typeof window !== 'undefined') {
-      // Use the Leaflet library and mapRef to initialize the map
+      const map = L.map(mapRef.current).setView([52.52, 13.405], 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      sampleLocations.forEach((location) => {
+        L.marker([location.lat, location.lng])
+          .addTo(map)
+          .bindPopup(`
+            <h3>${location.name}</h3>
+            <p>Foot Traffic: ${location.footTraffic}</p>
+            <p>Competitors: ${location.competitors}</p>
+            <p>Demographics: ${location.demographics}</p>
+          `);
+      });
+
+      return () => {
+        map.remove();
+      };
     }
-  }, []);
+  }, [sampleLocations]);
 
   return (
-    <MapContainer ref={mapRef} center={[52.52, 13.405]} zoom={13} className="h-[600px] rounded-lg">
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-
-      {/* Render sample location markers */}
-      {sampleLocations.map((location) => (
-        <Marker
-          key={location.id}
-          position={[location.lat, location.lng]}
-          eventHandlers={{
-            click: () => onLocationSelect(location),
-          }}
-        >
-          <Popup>
-            <h3>{location.name}</h3>
-            <p>Foot Traffic: {location.footTraffic}</p>
-            <p>Competitors: {location.competitors}</p>
-            <p>Demographics: {location.demographics}</p>
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
+    <div ref={mapRef} className="h-[600px] rounded-lg" />
   );
 };
 
