86c583fc17fdffc4356eb4882a100c725de7ad6a
Tarik Arslan <103arslan@gmail.com>
2025-01-25 03:19:45 +0100

    map func2

--- src/app/components/MapView.js
+++ src/app/components/MapView.js
@@ -1,11 +1,19 @@
 // src/components/MapView.js
+import { useEffect, useRef } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const MapView = ({ selectedLocation, onLocationSelect, sampleLocations }) => {
-  const center = [52.52, 13.405]; // Berlin coordinates
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    // This code will only run in the browser, not during server-side rendering
+    if (typeof window !== 'undefined') {
+      // Use the Leaflet library and mapRef to initialize the map
+    }
+  }, []);
 
   return (
-    <MapContainer center={center} zoom={13} className="h-[600px] rounded-lg">
+    <MapContainer ref={mapRef} center={[52.52, 13.405]} zoom={13} className="h-[600px] rounded-lg">
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
